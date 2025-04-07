dad0e342902f452605df5e5a45c81ab85878ed20
alicedev94 <alicedev94@gmail.com>
2025-04-07 16:54:28 -0400

    this is limitation in node for ssr

--- ssr-love/main.js
+++ ssr-love/main.js
@@ -1,1 +1,17 @@
-console.log("ssr-love")
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Configurar directorio para archivos estáticos
+//app.use(express.static(path.join(__dirname, 'ssr-love')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
