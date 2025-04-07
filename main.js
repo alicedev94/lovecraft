79b2fa9b86d4ef3012f102cbd152a4fe4f048694
alicedev94 <alicedev94@gmail.com>
2025-04-07 15:31:18 -0400

    This is the basic operation of a template compiler to HTML or a valid code for the browser.

--- main.js
+++ main.js
@@ -6,8 +6,32 @@ const data = {
 
 const renderTemplate = () => ({
     kk: `
-        <h1>${data.name}</h1>
-        <p>Edad: ${data.age}</p>
-        <p>Ciudad: ${data.city}</p>
+        h1 -> ${data.name}
+        p -> Edad: ${data.age}
+        p-> Ciudad: ${data.city}
+
+        a -> href: https://www.google.com
+        
+
+        img -> src: https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png
     `
 })
+
+// Expresión regular para transformar el formato
+const regex = /(\w+)\s*->\s*(.+)/g;
+
+const engineView = (template) => {
+    const html = template.kk
+        .split("\n")                    // Dividir por saltos de línea
+        .map(line => line.trim())       // Eliminar espacios al inicio/final
+        .filter(line => line !== "")    // Quitar líneas vacías
+        .map(line =>
+            line.replace(regex, "<$1>$2</$1>")  // Aplicar la regex
+        )
+        .join("\n");                    // Unir todo en un string HTML
+
+    return html
+}
+
+const result = engineView(renderTemplate())
+
