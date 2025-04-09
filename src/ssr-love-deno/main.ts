4f8c9dd93158af8c836ff87cb8322ababfb5f281
alicedev94 <alicedev94@gmail.com>
2025-04-09 10:40:36 -0400

    finish basic example in deno, oak and vue

--- src/ssr-love-deno/main.ts
+++ src/ssr-love-deno/main.ts
@@ -1,16 +1,53 @@
 import { Application } from "jsr:@oak/oak/application";
 import { Router } from "jsr:@oak/oak/router";
 
+const data = {
+    title: 'Deno, Oak, Vue',
+    description: 'Demo app for Deno, Oak, and Vue',
+    count: 0
+}
+
+const html = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Deno, Oak, Vue</title>
+            <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+        </head>
+        <body>
+            <div id="app">
+                <h1>{{ serverData.title }}</h1>
+                <p>{{ serverData.description }}</p>
+                
+                <div class="counter">
+                    <p>Counter: {{ count }}</p>
+                    <button @click="increment">Increment</button>
+                </div>
+            </div>
+
+            <script>
+                const { createApp } = Vue;
+                
+                createApp({
+                    data() {
+                        return {
+                            serverData: ${JSON.stringify(data)},
+                            count: ${data.count}
+                        }
+                    },
+                    methods: {
+                        increment() {
+                            this.count++;
+                        }
+                    }
+                }).mount('#app');
+            </script>
+        </body>
+    </html>
+`;
 const router = new Router();
 router.get("/", (ctx) => {
-    ctx.response.body = `<!DOCTYPE html>
-    <html>
-      <head><title>Hello oak!</title><head>
-      <body>
-        <h1>Hello oak!</h1>
-      </body>
-    </html>
-  `;
+    ctx.response.body = html;
 });
 
 const app = new Application();
